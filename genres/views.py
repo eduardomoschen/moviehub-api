af9ebd75cfbeb1139a1cdb0c7b032d9d6c68f809
Eduardo Moschen <eduardo.moschen28@gmail.com>
2023-11-09 11:49:28 -0300

    feat: adição da app Genres
    realizado o CRUD dos gêneros dos filmes

--- genres/views.py
+++ genres/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from genres.serializers import GenreSerializer
+from genres.models import Genre
 
-# Create your views here.
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
